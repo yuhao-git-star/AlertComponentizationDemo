04322287f147e6901cbface1e23cb3648e323eac
yuhaoyasuo <yasuoyuhao@gmail.com>
2019-09-13 10:51:52 +0800

    Initial Commit

---
+++ AlertComponentizationDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlertComponentizationDemo
+//
+//  Created by 陳囿豪 on 2019/9/13.
+//  Copyright © 2019 yasuoyuhao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
